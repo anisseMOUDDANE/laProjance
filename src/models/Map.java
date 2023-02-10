3373411b725407632dbcabee6b5cf86275ed0180
MOUDDANE Anisse <anisse.mouddane@alumni.univ-avignon.fr>
2023-02-10 15:04:32 +0100

    initial commit

---
+++ src/models/Map.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Map {
+	private static ArrayList<ArrayList<Character>> map;
+	
+	
+}
---
+++ src/projetDesignPattern/ProjetDesignPattern.java
@@ -0,0 +1,15 @@
+package projetDesignPattern;
+
+public class ProjetDesignPattern {
+
+	public static void main(String[] args) {
+		/**
+		 * Projet de desgin pattern : 
+		 * 	piste : obserever
+		 * 	
+		 */
+		System.out.println("projance");
+		
+	}
+
+}
