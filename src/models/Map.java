4d035c0e64eadaef5e6f9efc78aea5ee2b20ec96
Sidney Audibert <sidney.audibert@teads.tv>
2023-02-28 10:03:51 +0100

    la kiffance

--- src/models/Map.java
+++ src/models/Map.java
@@ -2,6 +2,7 @@ package models;
 import models.Case;
 
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * • Une carte
@@ -48,16 +49,18 @@ public class Map {
         scan.close();
     }
 
+
     // genere une methode d'affichage de la map
-    public void afficherMap() {
+    public void genererMap() {
         Scanner scanner = new Scanner(System.in);
         setTailleX();
         setTailleY();
         boolean stop = false;
         while(!stop) {
-            for (int j = 0; j < tailleY; j++) {
-                for (int k = 0; k < tailleX; k++) {
-                    System.out.print("-");
+            for (int i = 0; i < tailleX; i++) {
+                for (int j = 0; j < tailleY; j++) {
+                    this.map[i][j] = new Case(i, j, randomType());
+                    //System.out.print("-");
                 }
                 System.out.println();
             }
@@ -70,11 +73,39 @@ public class Map {
       effacerMap();
      }
 
+     public void refreshMap() {
+            
+     }
+
+     // faire une classe random avec dedans randomType et isBloquant
+
+     public TypeCase randomType() {
+        TypeCase caseType = null;
+        Random r = new Random();
+        double random = (r.nextInt(11)) / 10.0;
+        if (random <= 0.6) {
+             caseType = TypeCase.SOL;
+        } 
+        else if (random > 0.6 && random <= 0.7){
+            caseType = TypeCase.PONT;
+        }
+        else if (random > 0.7 && random <= 0.8){
+            caseType = TypeCase.ARBRE;
+        }
+        else if (random > 0.8 && random <= 0.9){
+            caseType = TypeCase.ROCHER;
+        }
+        else if (random > 0.9 && random <= 1){
+            caseType = TypeCase.EAU;
+        }
+        return caseType;
+    }
+
+
      public void effacerMap() {
         //Avant nouvelle position de chaque personnage 
       System.out.print("\033[H\033[2J");
       System.out.flush();
      }
-     
 
     }
---
+++ src/models/TypeCase.java
@@ -0,0 +1,5 @@
+package models;
+
+    public enum TypeCase {
+        SOL, PONT, ARBRE, ROCHER, EAU
+    }
