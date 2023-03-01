6d2d6f38bf13597117b0a49fa04fd8f0fa63783a
Sidney Audibert <sidney.audibert@teads.tv>
2023-03-01 15:07:53 +0100

    génération des cases correcte

--- src/models/Map.java
+++ src/models/Map.java
@@ -1,7 +1,12 @@
 package models;
 import models.Case;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
+import models.Enfant.EnfantStandard;
+import models.Enfant.EtatEnfant;
+import models.Enfant.TypeEnfant;
+import models.deplacement.Enfant;
 
 /**
  * • Une carte
@@ -11,8 +16,10 @@ public class Map {
     private int tailleX;
     private int tailleY;
     private Case[][] map;
+    public static Map instanceMap = null;
+    public ArrayList<EnfantStandard> enfants;
 
-    public Map() {
+    private Map() {
         setTailleX();
         setTailleY();
         this.map = new Case[tailleX][tailleY];
@@ -24,6 +31,13 @@ public class Map {
 
     public int getTailleY() {
         return tailleY;
+    }
+
+    public static Map getInstance() {
+        if (instanceMap == null) {
+          instanceMap = new Map();
+        }
+        return instanceMap;
     }
 
     public Case[][] getMap() {
@@ -48,75 +62,137 @@ public class Map {
         this.tailleY = y;
     }
 
-    public void genererMap() {
-        Scanner scanner = new Scanner(System.in);
-        boolean stop = false;
-        System.out.println(this.tailleX);
-        System.out.println(this.tailleY);
-        while(!stop) {
+    public void genererMap(Map map, ArrayList<EnfantStandard> listeEnfants, Ogre angel) {
+        //System.out.print(listeEnfants);
+        //Scanner scanner = new Scanner(System.in);
+        //boolean stop = false;
+        //System.out.println(this.tailleX);
+        //System.out.println(this.tailleY);
+        //while(!stop) {
+            this.enfants = listeEnfants;
             for (int i = 0; i < tailleX; i++) {
                 for (int j = 0; j < tailleY; j++) {
                     this.map[i][j] = new Case(i, j, randomType());
-                    System.out.print(getEmoji(this.map[i][j]));
-                    System.out.print(" ");
+                    /* 
+                    for(EnfantStandard enfant : listeEnfants) {
+                        if(this.map[i][j].getPosition().equals(enfant.getPosition())){
+                            this.map[i][j].setHoteEnfant(enfant);
+                            System.out.print(getEmoji(this.map[i][j]));
+                        }
+                    } */
+                        System.out.print(getEmoji(this.map[i][j]));
+                        System.out.print(" ");
                 }
                 System.out.println();
             }
-        System.out.println("Appuyez sur Entrée pour arrêter l'affichage");
-        if(scanner.nextLine().isEmpty()) {
-            stop = true;
-            scanner.close();
-        }
+        //System.out.println("Appuyez sur Entrée pour arrêter l'affichage");
+        //if(scanner.nextLine().isEmpty()) {
+          //  stop = true;
+          //  scanner.close();
+        //}
       }
-      effacerMap();
-     }
+      //effacerMap();
+     //}
 
-     public void refreshMap() {
-
+     public void refreshMap () {
+        for (int i = 0; i < tailleX; i++) {
+            for (int j = 0; j < tailleY; j++) {
+                for(EnfantStandard enfant : this.enfants) {
+                    if(this.map[i][j].getPosition().equals(enfant.getPosition())){
+                        this.map[i][j].setHoteEnfant(enfant);
+                        System.out.print(getEmoji(this.map[i][j]));
+                    }
+                }
+                    System.out.print(getEmoji(this.map[i][j]));
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
      }
 
      // faire une classe random avec dedans randomType et isBloquant
 
      public TypeCase randomType() {
         TypeCase caseType = null;
-        Random r = new Random();
-        double random = (r.nextInt(11)) / 10.0;
-        if (random <= 0.8) {
+        double random = Math.round(Math.random() * 100) / 100.0;
+        if (random <= 0.92) {
              caseType = TypeCase.SOL;
         }
-        else if (random > 0.8 && random <= 0.85){
+        else if (random > 0.92 && random <= 0.94){
             caseType = TypeCase.PONT;
         }
-        else if (random > 0.85 && random <= 0.9){
+        else if (random > 0.94 && random <= 0.96){
             caseType = TypeCase.ARBRE;
         }
-        else if (random > 0.9 && random <= 0.95){
+        else if (random > 0.96 && random <= 0.98){
             caseType = TypeCase.ROCHER;
         }
-        else if (random > 0.95 && random <= 1){
+        else if (random > 0.98 && random <= 1){
             caseType = TypeCase.EAU;
         }
         return caseType;
     }
 
-    public String getEmoji(Case myCase) {
+    public String getEmojiCase(Case myCase) {
+        
         String returnCase = null;
         if(myCase.getType() == TypeCase.SOL){
             returnCase = "🌫";
         }
         else if(myCase.getType() == TypeCase.PONT){
-            returnCase = "🥷";
+            returnCase = "🛤";
         }
         else if(myCase.getType() == TypeCase.ARBRE){
-            returnCase = "🫄";
+            returnCase = "🌶";
         }
         else if(myCase.getType() == TypeCase.ROCHER){
-            returnCase = "🫃";
+            returnCase = "🪨";
         }
         else if(myCase.getType() == TypeCase.EAU){
-            returnCase = "🫃";
+            returnCase = "🏖";
         }
         return returnCase;
+    }
+
+    public String getEmojiEnfant(EnfantStandard enfant) {
+        String returnEnfant = null;
+        if(enfant.getTypeEnfant() == TypeEnfant.AVEC_CHAPEAU){
+            returnEnfant = "🎩";
+        }
+        if(enfant.getTypeEnfant() == TypeEnfant.AVEC_INSTRUMENT){
+            returnEnfant = "🎸";
+        }
+        if(enfant.getTypeEnfant() == TypeEnfant.FILLE){
+            returnEnfant = "👧";
+        }
+        if(enfant.getTypeEnfant() == TypeEnfant.GARCON){
+            returnEnfant = "👦";
+        }
+        if(enfant.getTypeEnfant() == TypeEnfant.NORMAL){
+            returnEnfant = "🎩";
+        }
+        return returnEnfant;
+    }
+
+    public String getEmojiAngel(Case caseE) {
+        String returnAngel = "👹";
+        return returnAngel;
+    }
+
+    public String getEmojiDead(EnfantStandard enfant) {
+        String returnAngel = "👹";
+        return returnAngel;
+    }
+
+    public String getEmoji(Case myCase) {
+        if(myCase.getHoteEnfant() != null && myCase.getHoteEnfant().getEtat()==EtatEnfant.VIVANT){
+            return getEmojiEnfant(myCase.getHoteEnfant());
+        }
+        //else if(myCase.getHoteEnfant().getEtat()==EtatEnfant.TOMBE){}
+        else if(myCase.getHoteAngel() != null){
+            return getEmojiAngel(myCase);
+        }
+        else return getEmojiCase(myCase);
     }
 
      public void effacerMap() {
